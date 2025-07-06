285912d09af2c23d0a5d3c950f801cba09321a37
shefali1503 <shefalisharmaa4455@gmail.com>
2025-07-06 19:50:11 +0530

    Add starting js and started with new section

---
+++ script.js
@@ -0,0 +1,7 @@
+function SendMessage(){
+    //emergency button pressed notification will be sent to the contacts
+}
+
+document.querySelector(".emergency").addEventListener("onclick",SendMessage)
+
+
