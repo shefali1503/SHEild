781fad8bc177f3769e1541dc8d3203b0d399d953
shefali1503 <shefalisharmaa4455@gmail.com>
2025-07-06 20:31:07 +0530

    More features added to emergency contacts(form)qi

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-function SendMessage(){
+// for the emergency email 
+
+function SendMessage() {
+    alert("Emergency message sent")
     //emergency button pressed notification will be sent to the contacts
 }
 
-document.querySelector(".emergency").addEventListener("onclick",SendMessage)
+document.querySelector(".emergency").addEventListener("click", SendMessage)
 
 
+// for the emergency contact input
+function addEmergencyContact() {
+    let formcontent = ` <input type="text" placeholder="Enter contact name" >
+            <input type="text" placeholder="Enter the contact number" >
+            <input type="email" placeholder="Enter the contact email">
+            <div class="formActions">
+                 <button class="clearBtn" type="button">Clear</button>
+                <button class="submitBtn" type="submit">Input</button>
+            </div>
+            `;
+    let formsection = document.querySelector(".inputContact");
+    formsection.innerHTML = formcontent;
+
+}
+
+document.querySelector(".addButton").addEventListener("click", addEmergencyContact)
