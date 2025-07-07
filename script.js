e15d31ce7067dc0dc3d509dbbc667d5ce88321e7
shefali1503 <shefalisharmaa4455@gmail.com>
2025-07-07 14:58:58 +0530

    Added few features to the emergency contacts section

--- script.js
+++ script.js
@@ -10,17 +10,52 @@ document.querySelector(".emergency").addEventListener("click", SendMessage)
 
 // for the emergency contact input
 function addEmergencyContact() {
-    let formcontent = ` <input type="text" placeholder="Enter contact name" >
-            <input type="text" placeholder="Enter the contact number" >
-            <input type="email" placeholder="Enter the contact email">
+    let formContainer = document.querySelector(".inputContact");
+    if (!formContainer.querySelector("input")) {//checks if the inputContact has any input field yet or not
+        formContainer.innerHTML = ` <input type="text" class="nameInput"    placeholder="Enter contact name" required> 
+            <input type="tel" class="numberInput" placeholder="Enter the contact number" pattern="[0-9]+" required>
+            <input type="email" class="emailInput" placeholder="Enter the contact email" required>
             <div class="formActions">
-                 <button class="clearBtn" type="button">Clear</button>
+                <button class="clearBtn" type="button">Clear</button>
                 <button class="submitBtn" type="submit">Input</button>
             </div>
             `;
-    let formsection = document.querySelector(".inputContact");
-    formsection.innerHTML = formcontent;
 
+        //clear button to clear the form
+        document.querySelector(".clearBtn").addEventListener("click", function () {
+            //as the queryselectorAll will return the array
+            formContainer.querySelectorAll("input").forEach(input => input.value = '');
+        });
+
+        //submitbutton click
+        document.querySelector(".submitBtn").addEventListener("click", function (){
+            const name = document.querySelector(".nameInput").value.trim();
+            const number = document.querySelector(".numberInput").value.trim();
+            const email = document.querySelector(".emailInput").value.trim();
+
+            // Basic validation
+            const numberRegex = /^[0-9]+$/;
+            const emailRegex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+
+            //to check if all the field are filled or not
+            if (!name || !number || !email) {
+                alert("All fields are required.");
+                return;
+            }
+            //check the validity of the number and email id
+            if (!numberRegex.test(number)) {
+                alert("Contact number should contain digits only.");
+                return;
+            }
+
+            if (!emailRegex.test(email)) {
+                alert("Please enter a valid email address.");
+                return;
+            }
+
+        });
+    }
 }
-
 document.querySelector(".addButton").addEventListener("click", addEmergencyContact)
+
+
