80271075cfe81f189809c3b24e4aadc5712cf2db
shefali1503 <shefalisharmaa4455@gmail.com>
2025-08-04 17:17:15 +0530

    Added few changes

--- JS/script.js
+++ JS/script.js
@@ -499,15 +499,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-    document.addEventListener("DOMContentLoaded", function () {
-        const helplineLinks = document.querySelectorAll(".helpline-div a");
+document.addEventListener("DOMContentLoaded", function () {
+  const helplineLinks = document.querySelectorAll(".helpline-div a");
 
-        helplineLinks.forEach(link => {
-            link.addEventListener("click", function (event) {
-                const serviceName = link.textContent.trim();
-                const phoneNumber = link.getAttribute("href").replace("tel:", "");
-                alert(`You are about to call ${serviceName}\nPhone: ${phoneNumber}`);
-            });
-        });
+  helplineLinks.forEach(link => {
+    link.addEventListener("click", function (event) {
+      const serviceName = link.textContent.trim();
+      const phoneNumber = link.getAttribute("href").replace("tel:", "");
+      alert(`You are about to call ${serviceName}\nPhone: ${phoneNumber}`);
     });
-
+  });
+});
